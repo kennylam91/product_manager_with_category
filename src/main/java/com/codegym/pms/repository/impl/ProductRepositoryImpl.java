9ff094f950100d7a9406e3819988566235ab686c
kennylam91 <phamvanlam1991@gmail.com>
2019-07-03 17:00:48 +0700

    add more product to DB

--- src/main/java/com/codegym/pms/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/com/codegym/pms/repository/impl/ProductRepositoryImpl.java
@@ -14,11 +14,13 @@ public class ProductRepositoryImpl implements ProductRepository {
     public static Map<Integer,Category> categories;
     static{
         categories=new HashMap<>();
-        categories.put(1,new Category(1,"smart phone"));
+        categories.put(1,new Category(1,"smartphone"));
         categories.put(2, new Category(2,"laptop"));
         products=new HashMap<>();
         products.put(1,new Product(1,"Iphone XS",15000,15, categories.get(1)));
         products.put(2,new Product(2,"Thinkpad W541",22000,7, categories.get(2)));
+        products.put(3,new Product(3,"Samsung note9",20000,10, categories.get(1)));
+        products.put(4,new Product(4,"Macbook pro",35000,3, categories.get(2)));
 
     }
     @Override
