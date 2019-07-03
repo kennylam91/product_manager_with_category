11ae93fde2c9974687d7b1001858ade94482fa96
kennylam91 <phamvanlam1991@gmail.com>
2019-07-03 16:50:14 +0700

    Show list.html OK

--- src/main/java/com/codegym/pms/controller/ProductController.java
+++ src/main/java/com/codegym/pms/controller/ProductController.java
@@ -1,11 +1,14 @@
 package com.codegym.pms.controller;
 
+import com.codegym.pms.model.Product;
 import com.codegym.pms.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/product")
@@ -15,9 +18,8 @@ public class ProductController {
     ProductRepository productRepository;
     @GetMapping
     public ModelAndView getProductTable(){
-        String products = productRepository.findAll().toString();
         ModelAndView modelAndView = new ModelAndView("list");
-        modelAndView.addObject("products",products);
+        modelAndView.addObject("products",productRepository.findAll());
         return modelAndView;
 
     }
